b673fe08b684f890a634dd0eb85cebb2870483b0
ArielFeingold <feingold.ariel@gmail.com>
2018-05-07 15:31:41 -0400

    fixes to owner model

--- db/migrate/20180507192908_create_owners.rb
+++ db/migrate/20180507192908_create_owners.rb
@@ -4,7 +4,9 @@ class CreateOwners < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :city
       t.string :state
-
+      t.string :email
+      t.string :password_digest
+      
       t.timestamps
     end
   end
